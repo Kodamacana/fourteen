e457cade3778b81f02f004f690af7b0c675c6cf6
Talha Soylu <190905020@ogrenci.karatekin.edu.tr>
2022-04-18 17:39:59 +0300

    Scene0-v0.2

--- Fourteen/Assets/Scripts/Controller.cs
+++ Fourteen/Assets/Scripts/Controller.cs
@@ -6,6 +6,28 @@ namespace Controller
 {
     public class Controller : MonoBehaviour
     {
+        [SerializeField] DialogueManager dialogueManager;
+        [SerializeField] DialogueTrigger DialogueTrigger;
+        [SerializeField] Animator DialogueBoxAnim;
 
+
+        private void Start()
+        {
+            Coroutine();
+        }
+
+        public void Coroutine()
+        {
+            StartCoroutine(IE_DialogueTrigger());
+        }
+
+        IEnumerator IE_DialogueTrigger()
+        {
+            yield return new WaitForSecondsRealtime(2);
+
+            DialogueBoxAnim.gameObject.SetActive(true);
+            DialogueBoxAnim.SetBool("SlideStartAnim", true);
+            DialogueTrigger.TriggerDialogue();
+        }
     }
 }
