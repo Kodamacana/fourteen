09ae0d18e718810a9fa25e05cddaba46b9672285
Talha Soylu <63050957+Kodamacana@users.noreply.github.com>
2024-12-31 06:18:34 +0300

    Library bug fix

---
+++ Fourteen/Assets/Scripts/SwitchAnimation.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchAnimation : MonoBehaviour
+{
+    [SerializeField] Animator N;
+    [SerializeField] Animator T;
+    [SerializeField] GameObject GiftBox;
+
+    public void FallingGift()
+    {
+        GiftBox.SetActive(true);
+    }
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TrueRun()
+    {
+        T.SetBool("Run", true);
+        N.SetBool("Run", true);
+    }
+    public void TrueFall()
+    {
+        //T.SetBool("Jmp", true);
+        N.SetBool("Jmp", true);
+    }
+    public void TrueLand()
+    {
+        //T.SetBool("Land", true);
+        N.SetBool("Land", true);
+    }
+
+    public void FalseAllAnimation()
+    {
+        N.SetBool("Run", false);
+        N.SetBool("Land", false);
+        N.SetBool("Jmp", false);
+
+        T.SetBool("Run", false);
+        //T.SetBool("Land", false);
+        //T.SetBool("Jmp", false);
+    }
+}
